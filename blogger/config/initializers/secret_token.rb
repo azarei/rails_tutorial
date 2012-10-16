2bd5421a853832d2517cd921c86eb3350baed9e6
Anis Zarei <azarei@gettyimages.com>
2012-10-16 09:44:58 -0600

    Initial commit for Rails tutorial

---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = 'dba82a153762489748abde47c19f0b04a80c3e727b5affa688da94746c56d001a59aa8147d6e9fe318664b7b6956287e81da009f962db732b203d3335179e47c'
