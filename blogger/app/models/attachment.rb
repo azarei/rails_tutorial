2f150eedf68bca9b621429b3983f7ac59d96dda0
Anis Zarei <azarei@gettyimages.com>
2012-10-17 15:49:41 -0600

    Added a stylesheet & attaching images

--- blogger/app/models/attachment.rb
+++ blogger/app/models/attachment.rb
@@ -1,4 +1,4 @@
 class Attachment < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :title, :body, :image, :tag_list
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
