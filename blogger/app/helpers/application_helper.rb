a1fc5d5df4693a8553704638c7796cd33ecb4e08
Anis Zarei <azarei@gettyimages.com>
2012-10-22 16:07:14 -0600

    Added extra section for nav, and sorting by month

--- blogger/app/helpers/application_helper.rb
+++ blogger/app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def build_navigation
+    month_numbers = Article.all.collect{|a| a[:created_at].month}.uniq
+    month_links = '<ul id="side-menu">'
+    month_numbers.each do |m|
+      month_link = link_to Date::MONTHNAMES[m], articles_path(:type => 'month', :month => m)
+      month_links += '<li>' + month_link + '</li>'
+    end
+    month_links += '<li>' + (link_to "Most Popular", articles_path(:type => 'popular')) + '</li>'
+    month_links += '<li>' + (link_to "All Articles", articles_path) + '</li>'
+    month_links += '</ul>'
+    
+    return month_links.html_safe
+  end
 end
