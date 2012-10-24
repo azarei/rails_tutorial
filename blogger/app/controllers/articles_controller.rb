13ed3a21716c950986fcb57382705aa1a416e9aa
Anis Zarei <azarei@gettyimages.com>
2012-10-24 00:51:10 -0600

    Doing a bunch of changes
    Added some css changes, put in a few more tweaks.

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -2,16 +2,28 @@ class ArticlesController < ApplicationController
 	before_filter :require_login, :except => [:index, :show]
 
 	def index
-		@articles = Article.all		
+		@articles = Article.all
+
+		case params[:type]
+		when 'month'
+			if ((1..12)).include?(params[:month].to_i)
+				@articles = Article.by_month(params[:month].to_i, :field => :created_at)
+				@articles_displayed = Date::MONTHNAMES[params[:month].to_i]
+			end
+		end
 	end
+
 	def show
 		@article = Article.find(params[:id])
+	
 		@comment = Comment.new
 		@comment.article_id = @article.id
 	end
+	
 	def new
 		@article = Article.new
 	end
+	
 	def create
 		@article = Article.new (params[:article])
 		@article.save
@@ -20,6 +32,7 @@ class ArticlesController < ApplicationController
 
 		redirect_to article_path(@article)
 	end
+	
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
@@ -28,9 +41,11 @@ class ArticlesController < ApplicationController
 
 		redirect_to articles_path
 	end
+	
 	def edit
 		@article = Article.find(params[:id])
 	end
+	
 	def update
 		@article = Article.find(params[:id])
 		@article.update_attributes(params[:article])
