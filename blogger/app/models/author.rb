389ba42e5bc6ae786c9f3488c1209d8fb321ded1
Anis Zarei <azarei@gettyimages.com>
2012-10-24 11:51:11 -0600

    Revert "Revert "Doing a bunch of changes""
    This reverts commit a2d001b00be2164782943eb5d52cfe1b36253ab8.

--- blogger/app/models/author.rb
+++ blogger/app/models/author.rb
@@ -2,4 +2,6 @@ class Author < ActiveRecord::Base
   authenticates_with_sorcery!
   # attr_accessible :title, :body, :password, :message
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
+
+  has_many :articles
 end
